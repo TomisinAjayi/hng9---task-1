489fe80362f1025d20392d36d830aaa50a07dfce
TomisinAjayi <thomieajayi@gmail.com>
2022-10-25 17:50:43 +0100

    Web design completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import './css/Footer.css';
 const Footer = () => {
 
     return (
-        <div>
-            <h3>Zuri<span>.</span>Internship</h3>
+        <div className="footer">
+            <h3>Zuri<span className="red">.</span>Internship</h3>
             <p>HNG Internship 9 Frontend Task</p>
-
+            <div className="I4G-logo"></div>
         </div>
     );
 }
