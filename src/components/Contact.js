f3a7feba834b447489407e9720765fc19aaf694f
TomisinAjayi <thomieajayi@gmail.com>
2022-11-04 21:35:55 +0100

    web and mobile design completed.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
-import './css/Links.css';
+import './css/Contact.css';
 
 const Contact = () => {
+
+    const [values, setValues] = useState({message1: "",});
+    const [validate, setValidate] = useState({message1: "",});
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    }
+
+    const checkValidation = () => {
+        let errors = validate;
+
+        if (!values.message1.trim()) {
+            errors.message1 = "Please enter a message";
+        } else {
+            errors.message1 = "";
+        }
+
+        setValidate(errors);
+    }
+
+    useEffect(() => {
+        checkValidation();
+    }, [values]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <>
             <div className="container">
@@ -10,34 +39,35 @@ const Contact = () => {
                     <h3>Contact Me</h3>
                     <p>Hi there, contact me to ask me about anything you have in mind.</p>
                 </header>
-                <form method="POST" action="">
+                <form method="POST" action="/" onSubmit={handleSubmit}>
                     <div className="name-row">
                         <div className="fname">
                             <label htmlFor="first_name">First name</label>
                             <br />
-                            <input type="text" name="first_name" id="first_name" placeholder="Enter your first name" required />
+                            <input type="text" name="first_name" id="first_name" placeholder="Enter your first name" />
                         </div>
                         <div className="lname">
-                            <label htmlFor="last">Last name</label>
+                            <label htmlFor="last" id="lname">Last name</label>
                             <br />
-                            <input type="text" name="last" id="last_name" placeholder="Enter your last name" required />
+                            <input type="text" name="last" id="last_name" placeholder="Enter your last name" />
                         </div>
                     </div>
                     <div>
                         <label htmlFor="email">Email</label>
                         <br />
-                        <input type="email" id="email" name="email" placeholder="yourname@email.com" required />
+                        <input type="email" id="email" name="email" placeholder="yourname@email.com" />
                     </div>
                     <div>
                         <label htmlFor="message">Message</label>
                         <br />
-                        <textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+                        <textarea onChange={(e) => handleChange(e)} value={values.message1} id="message1" name="message1" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+                        {validate.message1 && <p className="errors">{validate.message1}</p>}
                     </div>
-                    <div>
+                    <div className="agreement">
                         <input type="checkbox" />
                         You agree to providing your data to who may contact you.
                     </div>
-                    <button id="btn__submit">
+                    <button type="submit" id="btn__submit">
                         Send message
                     </button>
                 </form>
